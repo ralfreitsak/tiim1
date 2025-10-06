b72f7810247b073ebcec19841b3283cc8be01cff
Ralf Reitsak <ralfreitsak@gmail.com>
2025-10-06 18:03:43 +0300

    message add post command

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
@@ -10,6 +12,14 @@ app.get('/:comment/:user', (req, res) => {
   res.send('Kommentaar: ' + req.params.comment + ' Nimi: ' + req.params.user);
 })
 
+app.post('/emotions', (req, res) => {
+    console.log("arrived at /emotions");
+    console.log("emotions: ", req.body);
+    const emotions = req.body;
+    const result = emotions.join(',');
+    res.send("Emotions received: " + result);
+})
+
 app.listen(port, () => {
-  console.log('Example app listening on port ${port}')
+  console.log('Example app listening on port', port)
 })
